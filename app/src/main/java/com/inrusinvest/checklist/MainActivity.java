7e27187a311ebe752f37d05917b607af0a702927
vgubarev <vladi-2005@mail.ru>
2018-10-05 17:07:10 +0700

    beginning

--- app/src/main/java/com/inrusinvest/checklist/MainActivity.java
+++ app/src/main/java/com/inrusinvest/checklist/MainActivity.java
@@ -1,8 +1,6 @@
 package com.inrusinvest.checklist;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,9 +8,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         String[] mass = new String[] {"Какая-то организация 1", "Какая-то организация 2", "Какая-то организация 3"};
 
 
-        ListView lv = (ListView) findViewById(R.id.parent_list_org);
+        ListView lv = findViewById(R.id.parent_list_org);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.list_item_view, mass);
         lv.setAdapter(arrayAdapter);
 
